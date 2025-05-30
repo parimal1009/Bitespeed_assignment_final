d8b4857377e13cd59f75e315cca666f8d131bcac
parimal1009 <118711970+parimal1009@users.noreply.github.com>
2025-05-30 21:11:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -297,7 +297,3 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 # Create the Vercel handler
 handler = app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8094)
